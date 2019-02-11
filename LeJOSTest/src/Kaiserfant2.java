acd95f4be762c7265d735de0690e3a77e9a7d7af
jakob.kaiser <jakob.kaiser@AMS-GYM-EDV1-14.gym.ams.local>
2019-02-11 17:58:19 +0100

    SA

--- LeJOSTest/src/Kaiserfant2.java
+++ LeJOSTest/src/Kaiserfant2.java
@@ -1,5 +1,12 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
@@ -9,41 +16,84 @@ public class Kaiserfant2 {
 
 		RegulatedMotor motorA = new EV3MediumRegulatedMotor(MotorPort.A);
 		RegulatedMotor motorD = new EV3MediumRegulatedMotor(MotorPort.D);
-		  
-		motorA.setSpeed(250);
-		motorA.backward();
 		
-		motorD.setSpeed(250);
+					
+		EV3 ev3 = (EV3) BrickFinder.getLocal();
+		TextLCD lcd = ev3.getTextLCD();
+	////	Keys keys = ev3.getKeys();
+
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+		SensorMode color = colorSensor.getColorIDMode();
+		float[] sample = new float[color.sampleSize()];
+		color.fetchSample(sample, 0);
+		int colorId = (int) sample[0];
+		String colorName = "";
+		switch (colorId) {
+		case Color.NONE:
+			colorName = "NONE";
+			break;
+		case Color.BLACK:
+			colorName = "BLACK";
+			break;
+		case Color.BLUE:
+			colorName = "BLUE";
+			break;
+		case Color.GREEN:
+			colorName = "GREEN";
+			break;
+		case Color.YELLOW:
+			colorName = "YELLOW";
+			break;
+		case Color.RED:
+			colorName = "RED";
+			break;
+		case Color.WHITE:
+			colorName = "WHITE";
+			break;
+		case Color.BROWN:
+			colorName = "BROWN";
+			break;
+		}
+		lcd.drawString(colorId + " - " + colorName, 0, 0);
+	////	keys.waitForAnyPress();
+		
+		motorA.setSpeed(300);
+		motorA.backward();
+
+		motorD.setSpeed(300);
 		motorD.backward();
-		 
+
 		Delay.msDelay(3000);
 		
-		motorA.setSpeed(0);
-		motorA.backward();
+	if (lcd == 0 );
+
 		
-		motorD.setSpeed(0);
-		motorD.backward();
-		 
-		Delay.msDelay(1000);
-		
-		motorA.setSpeed(250);
-		motorA.forward();
-		
-		motorD.setSpeed(250);
-		motorD.backward();
-		 
-		Delay.msDelay(30000);
-		
+//		
+//		motorA.setSpeed(0);
+//		motorA.backward();
+//		
+//		motorD.setSpeed(0);
+//		motorD.backward();
+//		 
+//		Delay.msDelay(1000);
+
+//		motorA.setSpeed(550);
+//		motorA.forward();
+//		
+//		motorD.setSpeed(550);
+//		motorD.backward();
+//		 
+//		Delay.msDelay(30000);
+
 		motorD.stop();
-		 
+
 		// ...
-		 
+
 		motorA.stop();
-		 
+
 		motorA.close();
-		 
+
 		motorD.close();
 
 	}
-
 }
